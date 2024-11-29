a8eed064dc12e454a882a48baee5fbbd57a66136
sumon-ohid <smd-ohid@student.42wolfsburg.de>
2024-11-29 08:23:52 +0100

    testing posgres, if not works come back here or 1 before

--- backend/pong/pong/settings.py
+++ backend/pong/pong/settings.py
@@ -74,11 +74,33 @@ WSGI_APPLICATION = 'pong.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+# cat myproject/settings.py
+
 DATABASES = {
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': 'pongdb',
+
+        'USER': 'msumon',
+
+        'PASSWORD': 'msumon1234',
+
+        'HOST': 'postgres',
+
+        'PORT': '5432',
+
     }
+
 }
 
 
