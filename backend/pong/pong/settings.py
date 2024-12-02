1efb5f57ea684e6d4a7be058dbb7291852e38987
Sumon Md Ohiduzzaman <117649754+sumon-ohid@users.noreply.github.com>
2024-12-02 22:23:19 +0100

    Update settings.py

--- backend/pong/pong/settings.py
+++ backend/pong/pong/settings.py
@@ -23,7 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-load_dotenv()
+dotenv_path = find_dotenv()
+load_dotenv(dotenv_path)
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
