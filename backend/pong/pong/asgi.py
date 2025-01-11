e03a0a11c164919181b1b92703db8ae26afefe93
andrijajovanovic98 <andrija.jovanovic1998@gmail.com>
2025-01-11 17:56:46 +0100

    chatfixing

--- backend/pong/pong/asgi.py
+++ backend/pong/pong/asgi.py
@@ -9,8 +9,10 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 
 import os
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pong.settings')
+
 import django
 django.setup()
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from django.core.asgi import get_asgi_application
